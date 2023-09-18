193c69173c7cf17de42dd9f18b1dc6936c5614d4
kamel_azizi <kamel_azizi@ext.connect-tech.sncf>
2023-09-18 17:24:05 +0200

    Ajout de la validation du code du participant

--- back/src/participant/participant.service.ts
+++ back/src/participant/participant.service.ts
@@ -31,4 +31,9 @@ export class ParticipantService {
     async getAllCompleted() {
         return this.participantModel.find({ isCompleted: true }).exec();
     }
+
+    async isValidCode(id: string, code: string): Promise<boolean> {
+        const participant = await this.participantModel.findById(id).exec();
+        return participant?.code === code;
+    }
 }
